c07429016ad1acdfe791067654f9f8be3a13d74b
Varuzhan Manucharyan <varuzh.manucharian@yandex.ru>
2025-05-22 11:30:05 +0300

    Выполнил задание 2.1.2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
