f660d5cef826a0008d7c6ad16047bb8c24c58d56
Varuzhan Manucharyan <varuzh.manucharian@yandex.ru>
2025-05-22 11:24:58 +0300

    Выполнил задание 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat beanCat = applicationContext.getBean("cat", Cat.class);
+        System.out.println(beanCat.getName());
+
+        Cat beanCat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(beanCat2.getName());
+
+        System.out.println("Сравнение бинов у HelloWorld: " + (bean == bean2));
+        System.out.println("Сравнение бинов у Cat: " + (beanCat == beanCat2));
     }
 }
